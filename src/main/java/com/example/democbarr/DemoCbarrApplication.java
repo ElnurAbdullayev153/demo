99ba735279458f08373d28ffa2a161281cb57a59
ElnurAbdullayev153 <elnur_825@mail.ru>
2025-02-12 03:42:26 +0400

    init commit

---
+++ src/main/java/com/example/democbarr/DemoCbarrApplication.java
@@ -0,0 +1,10 @@
+package com.example.democbarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCbarrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCbarrApplication.class, args
---
+++ src/test/java/com/example/democbarr/DemoCbarrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democbarr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCbarrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
